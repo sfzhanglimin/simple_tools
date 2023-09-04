91400e22bfb284ea21b159005ae2232dd09ef9a8
zlm <zlm>
2023-09-04 13:54:41 +0800

    delete

--- decode-creator/cocos2d/core/assets/material/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./CCEffectAsset");
-require("./CCMaterial");
-require("./material-variant");
--- decode-creator/cocos2d/renderer/gfx/misc.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isPow2 = void 0;
-function isPow2(v) {
-    return !(v & (v - 1)) && (!!v);
-}
-exports.isPow2 = isPow2;
